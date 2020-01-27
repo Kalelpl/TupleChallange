84deb6dc40f5ffe49a6e444e69794e67fea4d63b
Kalelpl <36899631+Kalelpl@users.noreply.github.com>
2020-01-27 15:08:50 +0100

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -9,3 +9,18 @@ print(artist)
 print(year)
 for i in tracks:
     print(i, end='\t')
+    
+    #2ga wersja
+    
+imelda = 'More Mayhem', "Imilda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"),(3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+
+title,artist,year,tracks = imelda
+
+print(title)
+print(artist)
+print(year)
+for song in tracks:
+    track, title = song
+    print("\tTrack number {}, Title: {}".format(track,title))
